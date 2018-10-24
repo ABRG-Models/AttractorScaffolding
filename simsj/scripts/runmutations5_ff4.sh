b093f72d733b8e8a484d64a18a62f4a14155abf6
Seb James <seb.james@sheffield.ac.uk>
2018-10-24 10:41:09 +0100

    Adds ff4 rummutations scripts

---
+++ simsj/scripts/runmutations5_ff4.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+PWD=`pwd | awk -F'/' '{ print $NF }'`
+if [ ${PWD} = 'scripts' ]; then
+    pushd ..
+elif [ ${PWD} = 'simsj' ]; then # This line will change
+    pushd .
+else
+    echo "Please run this from simsj or the scripts directory."
+fi
+
+# Find out where to run from.
+HN=`hostname`
+if [ ! -d ${HN} ]; then
+    if [ -d build ]; then
+        echo "No directory '${HN}'; defaulting to the directory 'build'"
+        HN=build
+    else
+        echo "Please build the software using cmake in a directory called '${HN}' or 'build'."
+        exit 1
+    fi
+fi
+
+# Run several mutations in parallel.
+./${HN}/sim/mutation5_ff4 01 &
+./${HN}/sim/mutation5_ff4 02 &
+./${HN}/sim/mutation5_ff4 03 &
+./${HN}/sim/mutation5_ff4 04 &
+./${HN}/sim/mutation5_ff4 05 &
+./${HN}/sim/mutation5_ff4 06 &
+./${HN}/sim/mutation5_ff4 07 &
+./${HN}/sim/mutation5_ff4 08 &
+./${HN}/sim/mutation5_ff4 09 &
+./${HN}/sim/mutation5_ff4 10 &
+
+wait
+popd
