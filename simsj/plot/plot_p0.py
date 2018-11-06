fd6aea84a3caf921b2fe9015523045a30b320a4a
Seb James <seb.james@sheffield.ac.uk>
2018-11-06 13:20:22 +0000

    Finished probability of zero/non-zero score in a single limit cycle compared with a single target state.

---
+++ simsj/plot/plot_p0.py
@@ -0,0 +1,104 @@
+import numpy as np
+import matplotlib
+matplotlib.use ('TKAgg', warn=False, force=True)
+import matplotlib.pyplot as plt
+import sys
+import csv
+
+# Read csv files.
+def readDataset (filepath):
+    numcols = 3
+    f = np.zeros([1,numcols])
+    with open (filepath, 'r') as csvfile:
+            rdr = csv.DictReader (csvfile)
+            for row in rdr:
+                f[-1,0] = float(row['l'])
+                f[-1,1] = float(row['p(!0)'])
+                f[-1,2] = float(row['p(0)'])
+                f = np.append(f, np.zeros([1,numcols]), 0)
+    # Note the -1 as there will be a final, zero line in the array
+    return f[:-1,:]
+
+files = ['../data/pnot0_n3.csv',
+         '../data/pnot0_n4.csv',
+         '../data/pnot0_n5.csv',
+         '../data/pnot0_n6.csv',
+         '../data/pnot0_n7.csv',
+         '../data/pnot0_n9.csv',
+         '../data/pnot0_n12.csv',
+         '../data/pnot0_n15.csv',
+         '../data/pnot0_n18.csv',
+         '../data/pnot0_n25.csv'
+]
+
+lbls = ['n=3',
+        'n=4',
+        'n=5',
+        'n=6',
+        'n=7',
+        'n=9',
+        'n=12',
+        'n=15',
+        'n=18',
+        'n=25'
+]
+
+mrkrs = ['o','s','v','^','h','o','s','v','^','h']
+
+# A nice colour array
+import sebcolour
+col = sebcolour.Colour
+cols1 = np.array([col.darkviolet,
+                  col.darkviolet,
+                  col.mediumorchid1,
+                  col.mediumorchid1,
+                  col.maroon1,
+                  col.maroon1,
+                  col.violetred,
+                  col.violetred,
+                  col.orange,
+                  col.orange,
+                  col.goldenrod1])
+
+# Font size for plotting
+fs=20
+fs2=18 # for axes tick labels
+
+# Set a default fontsize for matplotlib:
+fnt = {'family' : 'DejaVu Sans',
+       'weight' : 'regular',
+       'size'   : fs2}
+matplotlib.rc('font', **fnt)
+
+# F1 is a Figure instance
+F1 = plt.figure (figsize=(12,12))
+
+# fig1 is: <class 'matplotlib.axes._subplots.AxesSubplot'>
+fig1 = F1.add_subplot(121)
+fig2 = F1.add_subplot(122)
+
+nc = 0;
+for fl in files:
+    D = readDataset (fl);
+    fig1.plot (D[:,0],D[:,2], marker=mrkrs[nc], linestyle='-', color=cols1[nc], markersize=8)
+    fig2.plot (D[:,0],D[:,1], marker=mrkrs[nc], linestyle='-', color=cols1[nc], markersize=8)
+    nc = nc + 1
+
+fig1.legend(lbls, frameon=False)
+fig1.set_ylabel('Probability',fontsize=fs)
+fig1.set_xlabel('Limit cycle length',fontsize=fs)
+fig1.set_xlim([0,10])
+#fig1.set_ylim([1,9])
+fig1.set_axisbelow(True)
+fig1.set_title ("a) Probability of score 0");
+
+fig2.legend(lbls, frameon=False)
+#fig2.set_ylabel('$p(0)$',fontsize=fs)
+fig2.set_xlabel('Limit cycle length',fontsize=fs)
+fig2.set_xlim([0,10])
+#fig2.set_ylim([1,9])
+fig2.set_axisbelow(True)
+fig2.set_title ("b) Probability of score > 0");
+
+plt.savefig ('p0.png')
+plt.show()
--- simsj/sim/compute_pnot0.cpp
+++ simsj/sim/compute_pnot0.cpp
@@ -3,19 +3,18 @@
  * n.
  */
 #include <iostream>
-#include <map>
+#include <fstream>
+#include <sstream>
 
 #define N_Genes 5 // required for lib.h but unused
 #include "lib.h"
 
 #include "lmp.h"
-#include <sstream>
-#include <vector>
 
-double
+long double
 xint_to_double (Xint& xi)
 {
-    double d;
+    long double d;
     std::stringstream ss;
     ss << xi;
     ss >> d;
@@ -32,25 +31,37 @@ int main (int argc, char** argv)
     }
 
     int ngenes = atoi(argv[1]);
-    int n = (1<<ngenes);   // 2^ngenes
+    //int n = (1<<ngenes);   // 2^ngenes
 
-    for (int l = 1; l<=(1<<(ngenes-1)); ++l) {
+    ofstream fout;
+    stringstream fpath;
+    fpath << "./data/pnot0_n" << ngenes << ".csv";
+    fout.open (fpath.str().c_str(), ios::out|ios::trunc);
+
+    if (!fout.is_open()) {
+        cerr << "Failed to open file " << fpath.str() << endl;
+        return 1;
+    }
+
+    fout << "l,p(!0),p(0)" << endl;
+
+    long double p0 = 0.0;
+    long double pnot0 = 1.0;
+
+    for (int l = 1; l<=(1<<(ngenes)); ++l) {
 
         Xint two_to_n_choose_l;
         Xint two_to_n_over_2_choose_l;
         lmp::InitSetUi (two_to_n_choose_l, 1);
         lmp::InitSetUi (two_to_n_over_2_choose_l, 1);
 
-        double two_to_n_choose_l_d = 0.0;
-        double two_to_n_over_2_choose_l_d = 0.0;
-        double two_to_n_minus_1_over_2i_choose_l_d = 0.0;
-        double two_to_n_over_2i_choose_l_d = 0.0;
-        double m_choose_i_d = 0.0;
-        double numsum_d = 0.0;
-        double denomsum_d = 0.0;
-
-        double P0 = 0.0;
-        double PNOT = 1.0;
+        long double two_to_n_choose_l_d = 0.0;
+        long double two_to_n_over_2_choose_l_d = 0.0;
+        long double two_to_n_minus_1_over_2i_choose_l_d = 0.0;
+        long double two_to_n_over_2i_choose_l_d = 0.0;
+        long double m_choose_i_d = 0.0;
+        long double numsum_d = 0.0;
+        long double denomsum_d = 0.0;
 
         // Once only:
         lmp::BinomialUiUi(two_to_n_over_2_choose_l, 1<<(ngenes-1), l);
@@ -59,10 +70,10 @@ int main (int argc, char** argv)
         lmp::BinomialUiUi(two_to_n_choose_l, 1<<ngenes, l);
         two_to_n_choose_l_d = xint_to_double (two_to_n_choose_l);
 
-        double col0 = two_to_n_over_2_choose_l_d / two_to_n_choose_l_d;
-        PNOT = 1.0-col0;
+        long double col0 = two_to_n_over_2_choose_l_d / two_to_n_choose_l_d;
+        pnot0 = 1.0-col0;
         //cout << "P(ZC0) = " << col0 << endl;
-        //cout << "P(!ZC0) = " << PNOT << endl;
+        //cout << "P(!ZC0) = " << pnot0 << endl;
 
         for (ulong col = 1; col < ngenes; ++col) {
             numsum_d = 0.0;
@@ -124,14 +135,23 @@ int main (int argc, char** argv)
 
             // Compute probability
             //cout << "Probability sum: " << two_to_n_over_2_choose_l_d << " - " <<  numsum_d << " / " << two_to_n_choose_l_d << " - " << denomsum_d << endl;
-            double dd = (two_to_n_over_2_choose_l_d - numsum_d) / (two_to_n_choose_l_d - denomsum_d);
-            //cout << "P(ZC"<<(col)<<"|!ZC[1->"<<col<<"]) = " << dd << endl;
+            long double p_onezc = (two_to_n_over_2_choose_l_d - numsum_d) / (two_to_n_choose_l_d - denomsum_d);
+            //cout << "P(ZC"<<(col)<<"|!ZC[1->"<<col<<"]) = " << p_onezc << endl;
 
-            PNOT = PNOT * (1.0-dd);
+            pnot0 = pnot0 * (1.0-p_onezc);
         }
 
-        cout.precision(18);
-        cout << l << "," << PNOT << endl;
+        p0 = 1.0 - pnot0;
+
+        fout.precision(18);
+        fout << l << "," << pnot0 << "," << p0 << endl;
+
+        if (pnot0 == 1.0) {
+            break;
+        }
     }
+
+    fout.close();
+
     return 0;
 }
