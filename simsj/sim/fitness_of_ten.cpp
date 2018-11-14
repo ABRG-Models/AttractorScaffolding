0f9de84d323194123a77889083b60278a14d8190
Seb James <seb.james@sheffield.ac.uk>
2018-11-14 15:26:57 +0000

    Two new programs

---
+++ simsj/sim/fitness_of_ten.cpp
@@ -0,0 +1,84 @@
+/*
+ * Generate 10 non-zero fit genomes and show them, along with their
+ * fitness.
+ *
+ * Author: S James
+ * Date: November 2018.
+ */
+
+#include <iostream>
+#include <vector>
+#include <set>
+#include <sstream>
+#include <fstream>
+#include <string>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+using namespace std;
+
+// Choose debugging level. If you uncomment one of these, then you
+// should probably reduce N_Generations
+//
+// #define DEBUG 1
+// #define DEBUG2 1
+
+// Number of genes in a state is set at compile time.
+#ifndef N_Genes
+# define N_Genes 5
+#endif
+
+// The number of starting genomes to generate
+#ifndef N_Starts
+# define N_Starts 10
+#endif
+
+// Common code
+#include "lib.h"
+
+// The fitness function used here
+#include "fitness.h"
+
+// Perform a loop N_Generations long during which an initially
+// randomly selected genome is evolved until a maximally fit state is
+// achieved.
+int main (int argc, char** argv)
+{
+    // Seed the RNG.
+    unsigned int seed = mix(clock(), time(NULL), getpid());
+    srand (seed);
+
+    // Initialise masks
+    masks_init();
+
+    // Holds the starting genomes
+    array<array<genosect_t, N_Genes>, N_Starts> genomes;
+    // Store the fitnesses of all the starting genomes
+    array<double, N_Starts> initialFitness;
+
+    // Explicitly init to 0
+    for (unsigned int g = 0; g < N_Starts; ++g) {
+        initialFitness[g] = 0.0;
+    }
+
+    // Initialise genomes
+    for (unsigned int g = 0; g < N_Starts; ++g) {
+        while (initialFitness[g] == 0.0) {
+            random_genome (genomes[g]);
+            initialFitness[g] = evaluate_fitness (genomes[g]);
+        }
+    }
+
+    // Output fitness
+    cout.precision(12);
+    for (unsigned int g = 0; g < N_Starts; ++g) {
+        cout << genome2str(genomes[g]) << " has fitness " << initialFitness[g]
+#if defined SHOW_ID_ALSO
+             << " (" << genome_id(genomes[g]) << ")"
+#endif
+             << endl;
+    }
+
+    return 0;
+}
---
+++ simsj/sim/flip_stats.cpp
@@ -0,0 +1,78 @@
+/*
+ * Look at bit flipping stats.
+ *
+ * Author: S James
+ * Date: Nov 2018.
+ */
+
+#include <iostream>
+#include <vector>
+#include <set>
+#include <sstream>
+#include <fstream>
+#include <string>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+using namespace std;
+
+// Choose debugging level. If you uncomment one of these, then you
+// should probably reduce N_Generations
+//
+// #define DEBUG 1
+// #define DEBUG2 1
+
+// Number of genes in a state is set at compile time.
+#ifndef N_Genes
+# define N_Genes 5
+#endif
+
+// The number of generations to evolve for. 20000000 takes roughly 30
+// seconds on an Intel Core i9-8950HK CPU.
+#ifndef N_Generations
+# define N_Generations   100000000
+#endif
+
+// Whether to create large files with all the fitness information,
+// which can be used to make up graphs showing evolution of the
+// fitness.
+//#define RECORD_ALL_FITNESS 1
+
+// Common code
+#include "lib.h"
+
+// The fitness function used here
+#include "fitness.h"
+
+int main (int argc, char** argv)
+{
+    // Obtain pOn from command line.
+    if (argc < 2) {
+        LOG ("Usage: " << argv[0] << " pOn target_ant target_pos");
+        LOG ("Supply the probability of flipping a gene during evolution, pOn (float, 0 to 1.0f)");
+        LOG ("Optionally supply the anterior and posterior targets (integer, in range 0 to 31)");
+        return 1;
+    }
+    pOn = static_cast<float>(atof (argv[1]));
+    LOG ("Probability of flipping = " << pOn);
+
+    // Seed the RNG.
+    unsigned int seed = mix(clock(), time(NULL), getpid());
+    srand (seed);
+
+    // Initialise masks
+    masks_init();
+
+    // Holds the genome and a copy of it.
+    array<genosect_t, N_Genes> refg;
+
+    array<unsigned long long int, N_Genes> flipcount = {{ 0, 0, 0, 0, 0 }};
+    for (unsigned int i = 0; i < N_Generations; ++i) {
+        evolve_genome (refg, flipcount);
+    }
+    for (int j = 0; j < N_Genes; ++j) {
+        cout << "For pOn = " << pOn << " mean bits flipped in gene " << j<< " was " << flipcount[j] / (double)N_Generations << endl;
+    }
+    return 0;
+}
