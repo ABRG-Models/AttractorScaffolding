557fe85c3e34f4514afcc659cc06e634b9245e73
Seb James <seb.james@sheffield.ac.uk>
2018-11-14 15:32:52 +0000

    Updates to plotting evolution speed

--- simsj/plot/plot_evospeed_histo_only.py
+++ simsj/plot/plot_evospeed_histo_only.py
@@ -45,22 +45,18 @@ else:
              '../data/evolve_nodrift_a21_p10_ff4_100000000_gens_0.5.csv']
 
 
-lbls = ['p=0.10',
+lbls = ['p=0.05',
+        'p=0.10',
+        'p=0.15',
         'p=0.20',
+        'p=0.25',
         'p=0.30',
+        'p=0.35',
         'p=0.40',
+        'p=0.45',
         'p=0.50']
 
-lbls2 = ['p=0.05',
-         'p=0.10',
-         'p=0.15',
-         'p=0.20',
-         'p=0.25',
-         'p=0.30',
-         'p=0.35',
-         'p=0.40',
-         'p=0.45',
-         'p=0.50']
+print ('2nd element {0}'.format(lbls[2]))
 
 # num files
 nf = len(files)
@@ -83,7 +79,6 @@ matplotlib.rc('font', **fnt)
 scale = 1000.
 
 f1 = plt.figure(figsize=(20,6)) # Figure object
-a1 = f1.add_subplot (1,3,1)
 
 M = np.zeros([nf,3])
 
@@ -91,8 +86,9 @@ M = np.zeros([nf,3])
 nbins_g = 50
 
 # Plot the lines. These are what will appear in the legend
-printlines = 0
 fcount=0
+gcount=0
+a1 = []
 for y,fil in enumerate(files):
     print ('Processing file: {0}'.format(fil))
     fcount = fcount+1
@@ -112,15 +108,18 @@ for y,fil in enumerate(files):
         bx = b[x]/scale
         print ('b: {0}'.format(b))
         print ('h: {0}'.format(h))
-        fit = np.polyfit (bx, np.log(h[x]), 1)
+        fit = np.polyfit (np.log(bx), np.log(h[x]), 1)
         fit_fn = np.poly1d (fit)
 
         # Slope is fit[0], Record for a later graph.
         M[y,0] = fit[0]     # slope
         colo = plt.cm.plasma(fcount/10)
-        if (fcount%graphskip == 0) and printlines:
+        if (fcount%graphskip == 0):
             print ('Plotting file {0}'.format(fil))
-            a1.plot(bx,fit_fn(bx),'-',linewidth=2,color=colo)
+            ax = f1.add_subplot (2,5,gcount+1)
+            a1.append(ax)
+            a1[gcount].plot(np.log(bx),fit_fn(np.log(bx)),'-',linewidth=2,color=colo)
+            gcount = gcount + 1
     else:
         M[y,0] = 0     # no slope known
 
@@ -141,6 +140,7 @@ ms=[6,6,
     8,6]
 
 fcount=0
+gcount=0
 for y,fil in enumerate(files):
     print ('Processing file: {0}'.format(fil))
     fcount = fcount+1
@@ -154,62 +154,23 @@ for y,fil in enumerate(files):
 
     colo = plt.cm.plasma(fcount/10)
     if (fcount%graphskip == 0):
-        a1.plot(b[:-1]/scale,np.log(h),'.',color=colo,marker=mkr[y],markersize=ms[y])
+        a1[gcount].plot(np.log(b[:-1]/scale),np.log(h),'.',color=colo,marker=mkr[y],markersize=ms[y])
+        a1[gcount].set_title(lbls[gcount])
+        a1[gcount].set_ylabel(r'log (evolutions)',fontsize=fs)
+        a1[gcount].set_xlabel('1000 generations',fontsize=fs)
+        a1[gcount].set_axisbelow(True)
+        gcount = gcount + 1
 
-a1.legend(lbls2,frameon=False)
-a1.set_ylabel(r'log (evolutions) [' + driftnodrift + ']',fontsize=fs)
-a1.set_xlabel('1000 generations',fontsize=fs)
 ####a1.set_xlim([0,260])
 ####a1.set_ylim([-2,10])
 #a1.set_aspect(np.diff(a1.get_xlim())/np.diff(a1.get_ylim()))
-a1.set_axisbelow(True)
 #print (M)
 #f1.tight_layout()
 #plt.savefig ('distribution_evolutionary_histo_' + filetag + 'ff4.png')
 
-# Slope vs p fit. Fit line to most of the points.
-slope_fit = np.polyfit (M[1:,1], M[1:,0]/scale, 1)
-slope_fit_fn = np.poly1d (slope_fit)
 
-# Slope vs p
-#f2 = plt.figure(figsize=(8,8))
-a2 = f1.add_subplot (1,3,2)
-a2.plot (M[0,1], M[0,0]/scale, marker='h', linestyle='None', color=col.gray20, markersize=12)
-a2.plot (M[1:,1], M[1:,0]/scale, marker='o', linestyle='None', color=col.gray20, markersize=12)
-a2.plot (M[:,1], slope_fit_fn(M[:,1]), marker='None', linestyle='-', color=col.gray20, markersize=12)
-a2.set_xlabel('p')
-a2.set_ylabel('m (slope of best fit) [' + driftnodrift + ']')
-a2.set_xlim([0,0.55])
-a2.set_ylim([-0.001,0])
-#f2.tight_layout()
-#plt.savefig ('distribution_evolutionary_slope_vs_p_' + filetag + 'ff4.png')
-
-# Mean vs p fit. Fit exponential to the mean number of gens, excluding
-# the first point.
-log_means = np.log(M[1:,2])
-means_fit = np.polyfit (M[1:,1], log_means, 1)
-print ('Exponential fit to curve (' + driftnodrift + '): {0:.2f} exp ({1:.2f}*p)'.format(np.exp(means_fit[1]), means_fit[0]))
-expx = np.arange(0.01,0.54,0.001)
-expfit = np.exp (means_fit[1]) * np.exp (means_fit[0]*expx)
-
-
-# Mean vs p
-scale = 1 # change scale for this subplot
-#f3 = plt.figure(figsize=(8,8))
-a3 = f1.add_subplot (1,3,3)
-a3.plot (M[0,1], M[0,2]/scale, marker='h', linestyle='None', color=col.gray20, markersize=12)
-a3.plot (M[1:,1], M[1:,2]/scale, marker='o', linestyle='None', color=col.gray20, markersize=12)
-a3.plot (expx, expfit/scale, marker='None', linestyle='-', color=col.gray20, markersize=9)
-#a3 = f3.axes[0]
-a3.set_xlabel('p')
-a3.set_ylabel('$\mu$ (mean gens to evolve [' + driftnodrift + '] $f=1$)')
-a3.set_xlim([0,0.55])
-a3.set_ylim([0,80000])
-#f3.tight_layout()
-
-#plt.savefig ('distribution_evolutionary_mean_vs_p_' + filetag + 'ff4.png')
 f1.tight_layout()
-plt.savefig ('evolution' + filetag + '_ff4.png')
+plt.savefig ('evolution_histo_only' + filetag + '_ff4.png')
 
 plt.show()
 
