5a14022e8f04f0b7597f48f10697719368542e76
Seb James <seb.james@sheffield.ac.uk>
2019-01-09 17:49:31 +0000

    Updates to evolution speed plotting script

--- simsj/plot/plot_evospeed.py
+++ simsj/plot/plot_evospeed.py
@@ -20,50 +20,26 @@ def readDataset (filepath):
     # Note the -1 as there will be a final, zero line in the array
     return f[:-1,:]
 
+expfitstartidx = 4 # index of 0.05 in p, below
+p = [0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.557, 0.562, 0.6]#, 0.65, 0.7]
+
 maxgens='100000000'
-#maxgens='1000000000'
+
+# Make file names
+files = []
 if driftnodrift == 'drift':
     filetag = ''
-    files = ['../data/evolve_a21_p10_'+ff+'_'+maxgens+'_gens_0.05.csv',
-             '../data/evolve_a21_p10_'+ff+'_'+maxgens+'_gens_0.1.csv',
-             '../data/evolve_a21_p10_'+ff+'_'+maxgens+'_gens_0.15.csv',
-             '../data/evolve_a21_p10_'+ff+'_'+maxgens+'_gens_0.2.csv',
-             '../data/evolve_a21_p10_'+ff+'_'+maxgens+'_gens_0.25.csv',
-             '../data/evolve_a21_p10_'+ff+'_'+maxgens+'_gens_0.3.csv',
-             '../data/evolve_a21_p10_'+ff+'_'+maxgens+'_gens_0.35.csv',
-             '../data/evolve_a21_p10_'+ff+'_'+maxgens+'_gens_0.4.csv',
-             '../data/evolve_a21_p10_'+ff+'_'+maxgens+'_gens_0.45.csv',
-             '../data/evolve_a21_p10_'+ff+'_'+maxgens+'_gens_0.5.csv']
+    for pp in p:
+        files.append ('../data/evolve_a21_p10_{0}_{1}_gens_{2}.csv'.format(ff, maxgens, pp))
 else:
     filetag = '_nodrift'
-    files = ['../data/evolve_nodrift_a21_p10_'+ff+'_'+maxgens+'_gens_0.05.csv',
-             '../data/evolve_nodrift_a21_p10_'+ff+'_'+maxgens+'_gens_0.1.csv',
-             '../data/evolve_nodrift_a21_p10_'+ff+'_'+maxgens+'_gens_0.15.csv',
-             '../data/evolve_nodrift_a21_p10_'+ff+'_'+maxgens+'_gens_0.2.csv',
-             '../data/evolve_nodrift_a21_p10_'+ff+'_'+maxgens+'_gens_0.25.csv',
-             '../data/evolve_nodrift_a21_p10_'+ff+'_'+maxgens+'_gens_0.3.csv',
-             '../data/evolve_nodrift_a21_p10_'+ff+'_'+maxgens+'_gens_0.35.csv',
-             '../data/evolve_nodrift_a21_p10_'+ff+'_'+maxgens+'_gens_0.4.csv',
-             '../data/evolve_nodrift_a21_p10_'+ff+'_'+maxgens+'_gens_0.45.csv',
-             '../data/evolve_nodrift_a21_p10_'+ff+'_'+maxgens+'_gens_0.5.csv']
+    for pp in p:
+        files.append ('../data/evolve_nodrift_a21_p10_{0}_{1}_gens_{2}.csv'.format(ff, maxgens, pp))
 
-
-lbls = ['p=0.10',
-        'p=0.20',
-        'p=0.30',
-        'p=0.40',
-        'p=0.50']
-
-lbls2 = ['p=0.05',
-         'p=0.10',
-         'p=0.15',
-         'p=0.20',
-         'p=0.25',
-         'p=0.30',
-         'p=0.35',
-         'p=0.40',
-         'p=0.45',
-         'p=0.50']
+# Make labels
+lbls = []
+for pp in p:
+    lbls.append ('p={0}'.format(pp))
 
 # num files
 nf = len(files)
@@ -71,8 +47,6 @@ nf = len(files)
 # A nice colour array
 import sebcolour
 col = sebcolour.Colour
-
-graphskip = 2 # or 2 to skip every other.
 
 # Font size for plotting
 fs=16
@@ -103,12 +77,16 @@ for y,fil in enumerate(files):
     D = readDataset (fil)
     print ('D has rank {0}, shape {1} and size {2}'.format(D.ndim, D.shape, D.size))
     if D.size == 0:
+        print ('D size 0, continue...')
         continue
     bins = np.linspace(1,0.5*np.max(D),nbins)
     h,b = np.histogram (D, bins)
 
     # Do a fit
     x = np.where(np.log(h)>0)[0]
+    if x.size == 0:
+        print ('x size 0, continue...')
+        continue
     print ('x: {0}'.format(x))
     print ('x size: {0}'.format(x.size))
     if x.size > 0:
@@ -120,14 +98,14 @@ for y,fil in enumerate(files):
 
         # Slope is fit[0], Record for a later graph.
         M[y,0] = fit[0]     # slope
-        colo = plt.cm.brg((fcount*0.5)/len(files))
-        if (fcount%graphskip == 0) and printlines:
+        colo = plt.cm.brg((fcount)/len(files))
+        if printlines:
             print ('Plotting file {0}'.format(fil))
             a1.plot(bx,fit_fn(bx),'-',linewidth=2,color=colo)
     else:
         M[y,0] = 0     # no slope known
 
-    M[y,1] = (y+1)*0.05 # p, the flip probability.
+    M[y,1] = p[y] # p, the flip probability.
     M[y,2] = np.mean(D) # mean generations, in 10K
 
 
@@ -136,8 +114,18 @@ mkr=['.','o',
      'v','s',
      's','v',
      'o','^',
+     '^','h',
+     '.','o',
+     'v','s',
+     's','v',
+     'o','^',
      '^','h']
 ms=[6,6,
+    7,6,
+    7,7,
+    8,7,
+    8,6,
+    6,6,
     7,6,
     7,7,
     8,7,
@@ -150,16 +138,22 @@ for y,fil in enumerate(files):
     nbins = nbins_g
     D = readDataset (fil)
     if D.size == 0:
+        print ('D empty for file {0}'.format(fil))
         continue
     bins = np.linspace(1,0.5*np.max(D),nbins)
     h,b = np.histogram (D, bins)
+
+    # Do a fit just to see if we should include this one.
+    x = np.where(np.log(h)>0)[0]
+    if x.size == 0:
+        print ('x size 0, continue...')
+        continue
+
     # Plot points
+    colo = plt.cm.brg((fcount)/len(files))
+    a1.plot(b[:-1]/scale,np.log(h),'.',color=colo,marker=mkr[y],markersize=ms[y])
 
-    colo = plt.cm.brg((fcount*0.5)/len(files))
-    if (fcount%graphskip == 0):
-        a1.plot(b[:-1]/scale,np.log(h),'.',color=colo,marker=mkr[y],markersize=ms[y])
-
-a1.legend(lbls2,frameon=False)
+a1.legend(lbls,frameon=False)
 a1.set_ylabel(r'log (evolutions) [' + driftnodrift + ']',fontsize=fs)
 a1.set_xlabel('1000 generations',fontsize=fs)
 a1.set_axisbelow(True)
@@ -170,8 +164,9 @@ slope_fit_fn = np.poly1d (slope_fit)
 
 # Slope vs p
 a2 = f1.add_subplot (1,3,2)
-a2.plot (M[0,1], (M[0,0]/scale), marker='h', linestyle='None', color=col.gray20, markersize=12)
-a2.plot (M[1:,1], (M[1:,0]/scale), marker='o', linestyle='None', color=col.gray20, markersize=12)
+#a2.plot (M[0,1], (M[0,0]/scale), marker='h', linestyle='None', color=col.gray20, markersize=12)
+#a2.plot (M[1:,1], (M[1:,0]/scale), marker='o', linestyle='None', color=col.gray20, markersize=12)
+a2.plot (M[:,1], (M[:,0]/scale), marker='o', linestyle='None', color=col.gray20, markersize=12)
 ##a2.plot (M[:,1], slope_fit_fn(M[:,1]), marker='None', linestyle='-', color=col.gray20, markersize=12)
 a2.set_xlabel('p')
 a2.set_ylabel('m (slope of best fit) [' + driftnodrift + ']')
@@ -179,24 +174,27 @@ a2.set_ylabel('m (slope of best fit) [' + driftnodrift + ']')
 #a2.set_ylim([-0.001,0])
 
 # Mean vs p fit. Fit exponential to the mean number of gens, excluding
-# the first point.
-log_means = np.log(M[1:,2])
-means_fit = np.polyfit (M[1:,1], log_means, 1)
-print ('Exponential fit to curve (' + driftnodrift + '): {0:.2f} exp ({1:.2f}*p)'.format(np.exp(means_fit[1]), means_fit[0]))
-expx = np.arange(0.01,0.54,0.001)
+# the first couple of points
+log_means = np.log(M[expfitstartidx:,2])
+means_fit = np.polyfit (M[expfitstartidx:,1], log_means, 1)
+print ('Exponential fit to curve ({2}): {0:.2f} exp ({1:.2f}*p)'.format(np.exp(means_fit[1]), means_fit[0], driftnodrift))
+expx = np.arange(0.05,0.6,0.001)
 expfit = np.exp (means_fit[1]) * np.exp (means_fit[0]*expx)
 
 
 # Mean vs p
 scale = 1 # change scale for this subplot
 a3 = f1.add_subplot (1,3,3)
-a3.plot (M[0,1], M[0,2]/scale, marker='h', linestyle='None', color=col.gray20, markersize=12)
-a3.plot (M[1:,1], M[1:,2]/scale, marker='o', linestyle='None', color=col.gray20, markersize=12)
-a3.plot (expx, expfit/scale, marker='None', linestyle='-', color=col.gray20, markersize=9)
+#a3.plot (M[0,1], M[0,2]/scale, marker='h', linestyle='None', color=col.gray20, markersize=12)
+a3.plot (M[:,1], M[:,2]/scale, marker='o', linestyle='None', color=col.gray20, markersize=12)
+a3.plot (expx, expfit/scale, marker='None', linestyle='-', color=col.crimson, markersize=9)
+# Plot a point showing the mean number of f=1 genomes when randomly sampling
+a3.plot ([0,0.8], [146520,146520], marker='None', linestyle='-', color=col.navy)
+a3.legend(['Mean gens to evolve','Fit: {0:.2f} exp ({1:.2f}*p)'.format(np.exp(means_fit[1]), means_fit[0], driftnodrift),'Random discovery rate'])
 a3.set_xlabel('p')
 a3.set_ylabel('$\mu$ (mean gens to evolve [' + driftnodrift + '] $f=1$)')
-a3.set_xlim([0,0.55])
-a3.set_ylim([0,80000])
+a3.set_xlim([0,0.62])
+#a3.set_ylim([0,180000])
 
 f1.tight_layout()
 plt.savefig ('png/evolution' + filetag + '_'+ff+'.png')
