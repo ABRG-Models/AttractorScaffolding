88fd3d9b1e3c44ad2d9ea51c920999b9b4245372
Seb James <seb.james@sheffield.ac.uk>
2019-01-10 12:00:01 +0000

    Trivial comment changes

--- simsj/sim/endpoint.h
+++ simsj/sim/endpoint.h
@@ -1,14 +1,16 @@
 #ifndef _ENDPOINT_H_
 #define _ENDPOINT_H_
 
-// For fitness debugging.
+/*!
+ * For fitness debugging.
+ */
 #ifdef DEBUGF
 # define DBGF(s)  cout << "DBGF: " << __FUNCTION__ << ": " << s << endl;
 #else
 # define DBGF(s)
 #endif
 
-/*
+/*!
  * An enumerated type for the end point of a cycle. Used in fitness functions.
  */
 enum endpoint_t {
