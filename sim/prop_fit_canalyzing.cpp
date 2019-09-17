18f076ff1fab107dc0ed60e90e15d82aa5f6b825
Seb James <seb.james@sheffield.ac.uk>
2019-09-17 16:44:31 +0100

    How many f=1 genomes are canalysing (to some extent)

--- sim/prop_fit_canalyzing.cpp
+++ sim/prop_fit_canalyzing.cpp
@@ -1,31 +1,24 @@
 /*
- * Find the proportion of genomes that have non-zero canalyzingness.
- *
- * This defaults the N_Genes=5, but N_Genes can be defined on the
- * compiler command line to find the results for other values.
+ * Find the proportion of fit genomes which are canalyzing functions.
  *
  * Author: S James
  * Date: September 2019.
  */
 
 #include <iostream>
-#include <vector>
-#include <set>
+#include <map>
+#include <array>
 #include <stdlib.h>
-#include <sstream>
-#include <fstream>
 #include <string>
-#include <math.h>
 #include <sys/types.h>
 #include <unistd.h>
-#include <map>
 
 using namespace std;
 
 // Choose debugging level.
 //
-// #define DEBUGF 1
-// #define DEBUG 1
+//#define DEBUGF 1
+//#define DEBUG 1
 // #define DEBUG2 1
 
 // Number of genes in a state is set at compile time.
@@ -36,40 +29,50 @@ using namespace std;
 // Common code
 #include "lib.h"
 
-#include <climits>
+// The fitness function used here
+#include "fitness.h"
+#include "mutation.h"
 
 int main (int argc, char** argv)
 {
-    // Seed the RNG.
+    // Seed the RNG
+#define PROPERLY_RANDOM_SEED
+#ifdef PROPERLY_RANDOM_SEED
     unsigned int seed = mix(clock(), time(NULL), getpid());
+#else
+    unsigned int seed = 4;
+#endif
     srand (seed);
+
+    // Make the evolutions as fast as possible
+    pOn = 0.05;
 
     // Initialise masks
     masks_init();
 
-    // Unused, but set, in this program.
-    pOn = 0.5;
-
     // To store the results of this program
     map<unsigned int, unsigned int> canalvalues;
 
-    unsigned int ntrials = 10000000;
+    unsigned int ntrials = 100000;
 
     array<genosect_t, N_Genes > genome;
+
     for (unsigned int i = 0; i < ntrials; ++i) {
+        //cout << "+" << flush;
+        genome = evolve_new_genome ();
         random_genome (genome);
         unsigned int c = canalyzingness(genome);
         if (c > 0) {
             cout << "." << flush;
         }
-        if ((i % 100000) == 0) {
+        if ((i % 10000) == 0) {
             cout << i << flush;
         }
         canalvalues[c] += 1;
     }
 
     // Output results
-    cout << "Number of genomes tested: " << ntrials << endl;
+    cout << "Number of fit genomes tested: " << ntrials << endl;
     auto m = canalvalues.begin();
     while (m != canalvalues.end()) {
         cout << "Canalyzation value " << m->first << " seen " << m->second << " times" << endl;
