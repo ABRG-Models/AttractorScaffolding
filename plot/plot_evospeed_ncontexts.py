df917d1c7a7064bb674658cc88820bf9782c7dcd
Seb James <seb.james@sheffield.ac.uk>
2019-09-18 16:29:29 +0100

    Latest changes before hometime

--- plot/plot_evospeed_ncontexts.py
+++ plot/plot_evospeed_ncontexts.py
@@ -24,7 +24,14 @@ expfitstartidx = 4 # index of 0.05 in p, below
 p = [0.01, 0.02, 0.03, 0.04, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5]
 
 directry = 'dataj'
-contexttag = 'nc3_I16-4-1_T20-5-10'
+
+# 3 contexts
+#contexttag = 'nc3_I16-4-1_T20-5-10'
+# 4 contexts
+#contexttag = 'nc4_I16-4-1-8_T20-10-5-9'
+
+contexttag = 'nc4_I16-4-1-8_T20-10-5-9'
+
 maxgens='100000000'
 
 # Make file names
