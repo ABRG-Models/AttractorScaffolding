d88168c07ec1e16cc2b8982f7442c763968e2abe
Seb James <seb.james@sheffield.ac.uk>
2019-09-20 15:17:59 +0100

    Replace boilerplate with call to script_common.sh

--- scripts/runevolve_c3.sh
+++ scripts/runevolve_c3.sh
@@ -4,27 +4,7 @@
 # Evolve algo, n=5 genes, Target/Initial config from JSON
 #
 
-PWD=`pwd | awk -F'/' '{ print $NF }'`
-if [ ${PWD} = 'scripts' ]; then
-    pushd ..
-elif [ ${PWD} = 'AttractorScaffolding' ]; then # This line will change
-    pushd .
-else
-    echo "Please run this from AttractorScaffolding or the scripts directory."
-fi
-
-# Find out where to run from.
-HN=`hostname`
-if [ ! -d ${HN} ]; then
-    if [ -d build ]; then
-        echo "No directory '${HN}'; defaulting to the directory 'build'"
-        HN=build
-    else
-        echo "Please build the software using cmake in a directory called '${HN}' or 'build'."
-        exit 1
-    fi
-fi
-
+source script_common.sh
 echo "Using build directory ${HN} for executables"
 
 cat <<EOF > configs/runevolve_c3.json
