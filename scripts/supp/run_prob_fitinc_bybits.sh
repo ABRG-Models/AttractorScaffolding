2dc6985bd8b1e3a11125c48a9a7f8c093d8dedbb
Seb James <seb.james@sheffield.ac.uk>
2019-09-20 15:00:15 +0100

    More WIP in scripts

--- scripts/supp/run_prob_fitinc_bybits.sh
+++ scripts/supp/run_prob_fitinc_bybits.sh
@@ -24,14 +24,14 @@ fi
 echo "Using build directory ${HN} for executables"
 
 # Run several evolves in parallel.
-./${HN}/sim/prob_fitinc_bybits 3 &
-./${HN}/sim/prob_fitinc_bybits 4 &
-./${HN}/sim/prob_fitinc_bybits 5 &
-./${HN}/sim/prob_fitinc_bybits 6 &
-./${HN}/sim/prob_fitinc_bybits 8 &
-./${HN}/sim/prob_fitinc_bybits 10 &
-./${HN}/sim/prob_fitinc_bybits 15 &
-./${HN}/sim/prob_fitinc_bybits 20 &
+./${HN}/sim_supp/prob_fitinc_bybits 3 &
+./${HN}/sim_supp/prob_fitinc_bybits 4 &
+./${HN}/sim_supp/prob_fitinc_bybits 5 &
+./${HN}/sim_supp/prob_fitinc_bybits 6 &
+./${HN}/sim_supp/prob_fitinc_bybits 8 &
+./${HN}/sim_supp/prob_fitinc_bybits 10 &
+./${HN}/sim_supp/prob_fitinc_bybits 15 &
+./${HN}/sim_supp/prob_fitinc_bybits 20 &
 
 wait
 popd
